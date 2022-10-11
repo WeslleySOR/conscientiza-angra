1de4cd486370ef2a36a86927a8120de9f58b521f
WeslleySOR <weslleywruas@gmail.com>
2022-10-11 19:03:56 -0300

    feat: now header is position sticky and no fixed

--- src/components/AboutUsCard/index.tsx
+++ src/components/AboutUsCard/index.tsx
@@ -1,27 +1,13 @@
-import randomPic from '/assets/random-pic.jpg'
+import grupoDePessoas from '/assets/grupo-de-pessoas.png'
 
 export function AboutUsCard() {
   return (
-    <div className="flex flex-col gap-8 w-96 bg-[#4b7139]">
-      <img loading="lazy" src={randomPic} alt="Imagem aleatoria" />
-      <div className="flex flex-col gap-8">
-        <h1 className="text-center text-lg">Trung Thanh Nguyen - CEO</h1>
-        <p className="text-base">
-          Trung is involved in all key decisions from product to marketing and
-          leads the company in Vietnam.
-        </p>
-        <p className="text-base">
-          At the age of 19, Trung co-founded and was CTO of Lozi.vn, an early
-          Vietnamese e-commerce startup which has raised around 10 M in funding
-          and is still in operations as of now. He left Lozi once it became a
-          stable business and the team's focus shifted from building to
-          business/operations. Trung also had stints at Trusting Social (25 M
-          recent funding round led by Sequoia) and Anduin Transactions (a Joe
-          Lonsdale company) before founding Axie Infinity
-        </p>
-        <p className="text-base">
-          Trung represented Vietnam in the ACM-ICPC World Final 2014 held in
-          Yekaterinburg, Russia.
+    <div className="flex flex-col gap-8 w-full">
+      <img loading="lazy" src={grupoDePessoas} alt="Imagem aleatoria" />
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="text-center text-lg">Turma 3005 - Senac Angra dos Reis</h1>
+        <p className="text-base text-center">
+          Alunos: Ana Paula, Joao, Pedro, Lara, Miguel, Beatriz, Luiz.
         </p>
         <div className="flex justify-center items-center gap-4">
           <a className="underline text-base text-[#fff]" href="">Linkedin</a>
--- src/pages/AboutUs/index.tsx
+++ src/pages/AboutUs/index.tsx
@@ -2,19 +2,18 @@ import { AboutUsCard } from "../../components/AboutUsCard";
 
 export function AboutUs() {
   return (
-    <div className="scroll-mt-[152px] mt-8 flex flex-col bg-[#343434] text-[#fff] md:scroll-mt-[96px] lg:scroll-mt-[132px] xl:mx-12" id="sobre-nos">
+    <div className="scroll-mt-[152px] mt-8 flex flex-col bg-[#343434] text-[#fff] md:scroll-mt-[96px] lg:scroll-mt-[132px] xl:mx-12 lg:px-32" id="sobre-nos">
       <header className="flex items-center justify-center p-6">
         <h1 className="text-xl">Sobre nós</h1>
       </header>
-      <main className="flex flex-col py-16 px-6">
-        <span className="text-base">
+      <main className="flex flex-col py-8 lg:px-6">
+        <span className="text-base text-center">
           Somos um grupo de alunos, onde nosso objetivo é auxiliar na coleta
           seletiva do lixo, pensando sempre no melhor para a comunidade e a
           natureza.
         </span>
-        <div className="mt-16 flex justify-center flex-wrap gap-8">
-          <AboutUsCard />
-          <AboutUsCard />
+        {/* justify-center flex-wrap gap-8 */}
+        <div className="mt-8 flex">
           <AboutUsCard />
         </div>
       </main>
